2181a1b01af20268efbc518313e6a28c72e816d5
yamanx8 <yamanjazzar274@gmail.com>
2023-05-03 00:30:34 +0300

    initial react template

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./sass/style.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          vite official website
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className="text-purple-600" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
